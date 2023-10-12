8236c9032ae28e21417ff50930e3e9adf45459c1
Maksym <01178511@pw.edu.pl>
2023-10-12 17:05:27 +0200

    additional error catching in problem init, fix in calculate_gradient_value

--- lab1/src/problem.py
+++ lab1/src/problem.py
@@ -3,9 +3,14 @@ from numbers import Number
 
 
 class Problem:
-    def __init__(self, num_of_vars, function: Callable[[Tuple[Number]], Number],
+    def __init__(self, num_of_vars: int, function: Callable[[Tuple[Number]], Number],
                  gradient: Tuple[Callable[[Tuple[Number]], Number]]):
         # todo: validation whether num_of_vars equals number of parameters in function and gradient
+        if num_of_vars < 0:
+            raise ValueError("Number of variables can't be negative!")
+        if len(gradient) != num_of_vars:
+            raise ValueError("Number of elements in gradient has to be equal to the number of variables!")
+
         self._function = function
         self._gradient = gradient
         self._num_of_vars = num_of_vars
@@ -22,5 +27,4 @@ class Problem:
     def calculate_gradient_value(self, x0: Tuple[Number]) -> Tuple[Number, ...]:
         if len(x0) != self.num_of_vars:
             raise ValueError(f"Wrong number of variables in x0 ({len(x0)}). Required: {self.num_of_vars}.")
-        return tuple(self._gradient[i](x0[i]) for i in range(self.num_of_vars))
-
+        return tuple(self._gradient[i](x0) for i in range(self.num_of_vars))
