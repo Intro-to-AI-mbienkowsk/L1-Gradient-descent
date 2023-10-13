39fbbae608562c75a70c56c404d32220e3a88eb1
Maksym <01178511@pw.edu.pl>
2023-10-13 15:44:49 +0200

    switched from tuples to np.arrays so that arithmetic operations on the points are supported

--- lab1/src/problem.py
+++ lab1/src/problem.py
@@ -1,11 +1,17 @@
 from typing import Callable, Tuple
 from numbers import Number
+import numpy as np
 
 
 class Problem:
-    def __init__(self, num_of_variables: int, function: Callable[[Tuple[Number]], Number],
-                 gradient: Tuple[Callable[[Tuple[Number]], Number]]):
+    def __init__(self, num_of_variables: int, function: callable,
+                 gradient: tuple[callable] or callable):
         # todo: validation whether num_of_vars equals number of parameters in function and gradient
+
+        # convert single-dimensional gradients to a standard tuple of functions like in higher dimensions
+        if not isinstance(gradient, tuple):
+            gradient = tuple(gradient, )
+
         if num_of_variables < 0:
             raise ValueError("Number of variables can't be negative!")
         if len(gradient) != num_of_variables:
@@ -19,22 +25,29 @@ class Problem:
     def num_of_vars(self):
         return self._num_of_vars
 
-    def calculate_function_value(self, x0: Tuple[Number]) -> Number:
+    def calculate_function_value(self, x0: np.ndarray[Number] | Number) -> Number:
         """
         Calculates the value of the problem's function at a given point x0
         :param x0: point in the R^n space, n = len(x0)
         :return: the value of the function
         """
+        if not isinstance(x0, np.array):
+            x0 = np.array(x0)
+
         if len(x0) != self.num_of_vars:
-            raise ValueError(f"Wrong number of variables in x0 ({len(x0)}). Required: {self.num_of_vars}.")
+            raise ValueError(
+                f"Wrong number of variables in x0 ({len(x0)}). Required: {self.num_of_vars}.")
         return self._function(x0)
 
-    def calculate_gradient_value(self, x0: Tuple[Number]) -> Tuple[Number, ...]:
+    def calculate_gradient_value(self, x0: np.ndarray[Number] | Number) -> np.ndarray[Number]:
         """
         Calculates the value of the problem's gradient at a given point x0
         :param x0: point in the R^n space where n = len(x0)
         :return: the gradient of the function at x0, a tuple of length n
         """
-        if len(x0) != self.num_of_vars:
+        if not isinstance(x0, np.ndarray):
+            x0 = np.array(x0)
+
+        if x0.size != self.num_of_vars:
             raise ValueError(f"Wrong number of variables in x0 ({len(x0)}). Required: {self.num_of_vars}.")
-        return tuple(self._gradient[i](x0) for i in range(self.num_of_vars))
+        return np.array([self._gradient[i](x0) for i in range(self.num_of_vars)])
