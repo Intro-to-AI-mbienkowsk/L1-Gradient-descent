550eb50a474c946a7eec3de4653ff15f432a5ac0
Maksym <01178511@pw.edu.pl>
2023-10-18 23:05:53 +0200

    plotter now draws 3d plots

---
+++ lab1/src/Plotter.py
@@ -0,0 +1,29 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from problem import Problem
+
+
+class Plotter:
+    def __init__(self, problem: Problem):
+        if problem.num_of_vars > 2:
+            raise ValueError("Maximum number of dimensions supported by the plotter is 3.")
+        self._problem = problem
+        self._dimensions = self._problem.num_of_vars + 1
+
+        self.x_values = np.arange(-5, 5, .001)
+        self._fig = plt.figure()
+
+        if self._dimensions == 3:
+            self.y_values = np.arange(-5, 5, .001)
+
+    def initialize_plot(self):
+        X, Y = np.meshgrid(self.x_values, self.y_values)
+        Z = self._problem.calculate_function_value(np.array([X, Y]))
+        ax = self._fig.add_subplot(111, projection='3d')
+        ax.plot_surface(X, Y, Z, cmap='viridis')
+        ax.set_xlabel('x1')
+        ax.set_ylabel('x2')
+        ax.set_zlabel('f(x1, x2')
+        plt.show()
+
+
