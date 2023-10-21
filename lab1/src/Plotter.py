81718b55700a8e30b43ea741262106be8716f53f
Maksym <01178511@pw.edu.pl>
2023-10-21 13:19:02 +0200

    refactoring of plotter + it now supports drawing 2d functions

--- lab1/src/Plotter.py
+++ lab1/src/Plotter.py
@@ -9,36 +9,30 @@ class Plotter:
             raise ValueError("Maximum number of dimensions supported by the plotter is 3.")
         self._problem = problem
         self._dimensions = self._problem.num_of_vars + 1
+        self._ax = plt.figure().add_subplot(111, projection='3d' if self._dimensions == 3 else None)
 
-        self.x_values = np.arange(-5, 5, .001)
-        self._fig = plt.figure()
-
-        if self._dimensions == 3:
-            self.y_values = np.arange(-5, 5, .001)
         self._solving_path = []
 
     def initialize_plot(self):
+        arg_values = np.arange(-5, 5, .001)
         if self._dimensions == 3:
-            X, Y = np.meshgrid(self.x_values, self.y_values)
+            X, Y = np.meshgrid(arg_values, arg_values)
             Z = self._problem.calculate_function_value(np.array([X, Y]))
-            ax = self._fig.add_subplot(111, projection='3d')
-            ax.plot_surface(X, Y, Z, cmap='viridis')
-            ax.set_xlabel('x1')
-            ax.set_ylabel('x2')
-            ax.set_zlabel('f(x1, x2')
+            self._ax.plot_surface(X, Y, Z, cmap='viridis')
+            self._ax.set_xlabel('x1')
+            self._ax.set_ylabel('x2')
+            self._ax.set_zlabel('g(x1, x2)')
+
         else:
-            pass
+            Y = self._problem.calculate_function_value(arg_values)
+            self._ax.plot(arg_values, Y)
+            self._ax.set_xlabel('x')
+            self._ax.set_ylabel('f(x)')
+
+        plt.show()
 
     def add_point(self, x):
         self._solving_path.append(x)
 
-
     def update_plot(self):
-        plt.scatter(self.sol)
-
-
-        plt.show()
-
-
-
-
+        pass
